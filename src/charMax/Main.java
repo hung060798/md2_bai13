dba5fc3dbd8b04f31f107e3e3aa9f79c62d3f7d7
hung060798 <hungpppp122gmail.com>
2021-06-23 21:26:02 +0700

    chuoiMax

---
+++ src/charMax/Main.java
@@ -0,0 +1,33 @@
+package charMax;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Character> max = new LinkedList<>();
+        LinkedList<Character> list = new LinkedList<>();
+
+        System.out.println("nhap chuoi");
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++){
+            if (list.size() > 1 && str.charAt(i) <= list.getLast() && list.contains(str.charAt(i))){
+                list.clear();
+            }
+            list.add(str.charAt(i));
+
+            if (list.size() > max.size()){
+                max.clear();
+                max.addAll(list);
+            }
+        }
+
+        for (Character ch : max){
+            System.out.println(ch);
+        }
+        System.out.println();
+    }
+
+}
